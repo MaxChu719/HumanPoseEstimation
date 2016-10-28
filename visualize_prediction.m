ae214e71f1c7291948668e2702f8172b912bf4c9
MaxChu719 <chu.shekwai@gmail.com>
2016-10-29 03:48:28 +1100

    fix bug

--- visualize_prediction.m
+++ visualize_prediction.m
@@ -9,7 +9,7 @@ for b = 1:batch_size
     test_image = imread(im_name{b});
     im = single(test_image);
     for c = 1:3
-        im(:,:,c) = im(:,:,c) + imdb.images.normalization.average(c);
+        im(:,:,c) = im(:,:,c) - imdb.images.normalization.average(c);
     end
     res = vl_simplenn(net, im) ;
     input_joint = ones(14*3, 1);
